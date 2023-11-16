1d2803c5820087cf008357d0c7ea5c1a48217660
p. schuhmacher <contact@pschuhmacher.com>
2023-11-16 12:23:57 +0100

    propsoe un modele basic de controler

---
+++ app/controllers/Controler.php
@@ -0,0 +1,16 @@
+<?php
+
+class Controler
+{
+
+    public function execute()
+    {
+        //Do stuff...
+        //Genere une réponse au format HTML par exemple
+        $this->render();
+    }
+
+    public function render()
+    {
+    }
+}
